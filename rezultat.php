5e89d016a11e38f37b26f4ca2cafe2022e1751ee
patrikmarton1 <patrik.marton@skole.hr>
2017-11-15 12:55:34 +0100

    Dodali smo nove datoteke

---
+++ rezultat.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="style.css" </head>
+
+    <body>
+        <?php
+            //print_r($_POST);
+            echo('Pozdrav, ' . $_POST['prezime'] . $_POST['ime'])
+        ?>
+    </body>
+
+</html>
---
+++ unos.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Unos</title>
+    <link rel="stylesheet" type="text/css" href="style.css" 
+</head>
+
+<body>
+    <form action="rezultat.php" method="post">
+        Ime:<br>
+        <input type="text" name="ime"><br>
+
+        Prezime:<br>
+        <input type="text" name="prezime"><br>
+
+        <input type="submit" value="Pošalji!">
+    </form>
+</body>
+
+</html>
